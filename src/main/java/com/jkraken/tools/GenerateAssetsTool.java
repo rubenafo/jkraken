b68d772c55a222fc80e1382df219a990beb95977
Ruben Afonso <rbfrancos@gmail.com>
2020-01-25 10:17:00 +0000

    Generate AssetPairs

--- src/main/java/com/jkraken/tools/GenerateAssetsTool.java
+++ src/main/java/com/jkraken/tools/GenerateAssetsTool.java
@@ -1,5 +1,7 @@
 package com.jkraken.tools;
 
+import com.jkraken.api.JKraken;
+import com.jkraken.entities.AssetPairs;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeName;
@@ -8,14 +10,13 @@ import lombok.SneakyThrows;
 import lombok.var;
 
 import javax.lang.model.element.Modifier;
-import java.lang.reflect.Type;
+import java.nio.file.Paths;
 
 public class GenerateAssetsTool {
 
     @SneakyThrows
-    public static void main (String ...args) {
-
-                var enumType = TypeSpec.enumBuilder("AssetPairs")
+    private static void createEnum(AssetPairs assetPairs) {
+                var enumTypeBuilder = TypeSpec.enumBuilder("AssetPairs")
                         .addModifiers(Modifier.PUBLIC)
                         .addField(String.class, "altName")
                         .addField(String.class, "wsName")
@@ -24,17 +25,17 @@ public class GenerateAssetsTool {
                         .addField(String.class, "base")
                         .addField(String.class, "quote")
                         .addField(String.class, "lot")
+                        .addField(String.class, "feeVolCurrency")
                         .addField(TypeName.INT, "pairDecimal")
                         .addField(TypeName.INT, "lotDecimal")
                         .addField(TypeName.INT, "lotMultiplier")
                         .addField(TypeName.INT, "marginCall")
                         .addField(TypeName.INT, "marginStop")
-                        .addField(TypeName.INT, "leverageBuy")
                         .addMethod(MethodSpec.constructorBuilder()
                                 .addParameter(String.class, "altName").addStatement("this.$N = $N", "altName", "altName")
                                 .addParameter(String.class, "wsName").addStatement("this.$N = $N", "wsName", "wsName")
-                                .addParameter(String.class,        "aclassBase").addStatement("this.$N = $N", "aClassBase", "aClassBase")
-                                .addParameter(String.class,        "aclassQuote").addStatement("this.$N = $N", "aClassQuote", "aClassQuote")
+                                .addParameter(String.class,        "aClassBase").addStatement("this.$N = $N", "aClassBase", "aClassBase")
+                                .addParameter(String.class,        "aClassQuote").addStatement("this.$N = $N", "aClassQuote", "aClassQuote")
                                 .addParameter(String.class,        "quote").addStatement("this.$N = $N", "quote", "quote")
                                 .addParameter(String.class,        "lot").addStatement("this.$N = $N", "lot", "lot")
                                 .addParameter(String.class,        "base").addStatement("this.$N = $N", "base", "base")
@@ -44,22 +45,36 @@ public class GenerateAssetsTool {
                                 .addParameter(TypeName.INT, "lotMultiplier").addStatement("this.$N = $N", "lotMultiplier", "lotMultiplier")
                                 .addParameter(TypeName.INT, "marginCall").addStatement("this.$N = $N", "marginCall", "marginCall")
                                 .addParameter(TypeName.INT, "marginStop").addStatement("this.$N = $N", "marginStop", "marginStop")
-                                .addParameter(TypeName.OBJECT, "leverageBuy").addStatement("this.$N = $N", "leverageBuy", "leverageBuy")
-                        .build())
-                        .addModifiers()
-                        .addEnumConstant("ADAETH",
-                                TypeSpec.anonymousClassBuilder("$S, $S, $S, $S, $S, $S, $S, $S $L, $L, $L, $L, $L","ADAETH", "ADA/ETH", "currency", "ADA", "currency", "XETH", "unit", "ZUSD", 1,1,1,1,1)
-                                        .build())
-                .build();
+                        .build());
 
-        TypeSpec assetEnums = TypeSpec.classBuilder("AssetEnums")
+                // create constants
+                assetPairs.getResult().entrySet().forEach(entry -> {
+                    var pairName = entry.getKey();
+                    pairName = pairName.replace(".", "_");
+                    var attrs = entry.getValue();
+                    enumTypeBuilder.addEnumConstant(pairName,
+                            TypeSpec.anonymousClassBuilder("$S, $S, $S, $S, $S, $S, $S, $S, $L, $L, $L, $L, $L",
+                                    attrs.getAltname(), attrs.getWsname(), attrs.getAclass_base(), attrs.getBase(), attrs.getAclass_quote(), attrs.getQuote(), attrs.getLot(),
+                                    attrs.getFee_volume_currency(), attrs.getPair_decimals(), attrs.getLot_decimals(), attrs.getLot_multiplier(), attrs.getMargin_call(), attrs.getMargin_stop())
+                                    .build());
+                });
+
+
+        TypeSpec assetEnums = TypeSpec.classBuilder("AssetPairsEnum")
                 .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                .addType(enumType)
+                .addType(enumTypeBuilder.build())
                .build();
 
-        JavaFile javaFile = JavaFile.builder("com.jkraken.entities", assetEnums)
-                .build();
+        JavaFile javaFile = JavaFile.builder("com.jkraken.entities", assetEnums).build();
 
-        javaFile.writeTo(System.out);
+        System.out.println("Creating AssetPairsEnum.java file under com.jkraken.entities...");
+        javaFile.writeToPath(Paths.get("src/main/java/"));
+
     };
+
+    @SneakyThrows
+    public static void main (String ...args) {
+        var assetPairs = JKraken.getAssetPairs();
+        GenerateAssetsTool.createEnum(assetPairs);
+    }
 }
